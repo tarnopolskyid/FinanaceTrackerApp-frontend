218fef8fd77a30e2423db8aac495e479bc37922c
tarnoposkyi <tarnopolskyid@gmail.com>
2024-10-27 19:23:33 +0100

    generate components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component'
+import { LoginComponent } from './pages/login/login.component'
+import { SignupComponent } from './pages/signup/signup.component'
+import { ProfileComponent } from './pages/profile/profile.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    title: 'Signup'
+  },
+  {
+    path: "**",
+    component: HomeComponent,
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TransactionsFormComponent } from './components/transactions-form/transactions-form.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    SignupComponent,
+    HeaderComponent,
+    CategoriesComponent,
+    TransactionsFormComponent,
+    TransactionsTableComponent
   ],
   imports: [
     BrowserModule,
